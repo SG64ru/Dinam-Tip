42e81fcd0761a06ffd6e7daba929d695df582aef
SG64ru <sergg64@gmail.com>
2024-09-09 22:13:37 +0500

    Add files via upload

---
+++ Динамическая типизация.py
@@ -0,0 +1,16 @@
+name = "Сергей"
+print (name)
+age = 60
+print (age)
+new_age = age + 1
+print (new_age)
+is_student = True
+print (True)
+name = "Сергей"
+print ("Name: " + name)
+age = 60
+print ("Age: " + str(age))
+new_age = age + 1
+print ("new_age: " + str(new_age))
+is_student = True
+print ("is_student" + ": " + str (True))
